93a96ee6539745046054db1a95094da91b936e1a
Ousama Beaj <obeaj@e3r3p5.1337.ma>
2022-08-11 15:41:32 +0100

    modif

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#define SCALE 8
-#define Float_Fixed(x) roundf((float)x * (float)(1 << SCALE))
-#define Fixed_Float(x) ((float)x / (float)(1<<SCALE))
-int main()
-{
-    int a = Float_Fixed(14.42f);
-    std::cout<<Fixed_Float(a)<<std::endl;
-}
